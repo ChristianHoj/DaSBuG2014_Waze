2d77ddd1f43de39141e678f111c2cde465afaea2
Christian Høj <cbh@ucn.dk>
2014-04-08 19:03:14 +0300

    Take file encoding into account and report progress during conversion.

--- convert_waze_data.rb
+++ convert_waze_data.rb
@@ -14,13 +14,15 @@ abort usage if ARGV.length != 3
 
 mercator_file = ARGV[0]
 wgs84_out_file = ARGV[1]
-col = ARGV[2]
+col = ARGV[2].to_i
 tmp_file = "proj_tmp"
 tmp_wgs84_file = "proj_wgs84_tmp"
 
 # Extract locations from input file and write them to a temporary file
+line = 1
 File.open(tmp_file, "w") do |f|
-  CSV.foreach mercator_file, {col_sep:"\t"} do |row|
+  CSV.foreach mercator_file, {col_sep:"\t", encoding:"BOM|UTF-16LE:UTF-8"} do |row|
+    print "Reading line #{line}\r"
     if row[col].nil? || !row[col].include?(',')
       f.puts "\n"
     else
@@ -28,16 +30,20 @@ File.open(tmp_file, "w") do |f|
       row2 = row[col+1].sub(',', '')
       f.puts "#{row1}\t#{row2}\n"
     end
+    line += 1
   end
 end
+print "\n"
 
+puts "Converting coordinates using Proj.4"
 # Invoke the proj.4 command to convert from the Waze Mercator projection to WGS84 used by Leaflet, Google maps and others
 %x{proj +proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs -I -f '%.9f' #{tmp_file} > #{tmp_wgs84_file}}
 
+puts "Writing results"
 line = 0
 converted = CSV.read tmp_wgs84_file, {col_sep:"\t"}
 CSV.open(wgs84_out_file, "w", {col_sep:"\t"}) do |outfile|
-  CSV.foreach mercator_file, {col_sep:"\t"} do |row|
+  CSV.foreach mercator_file, {col_sep:"\t", encoding:"BOM|UTF-16LE:UTF-8"} do |row|
     if !row[col].nil? && row[col].include?(',')
       row[col] = converted[line][0]
       row[col+1] = converted[line][1]
@@ -49,5 +55,5 @@ end
 
 
 # Remove the temporary files
-# File.delete tmp_file
-# File.delete tmp_wgs84_file
+File.delete tmp_file
+File.delete tmp_wgs84_file
