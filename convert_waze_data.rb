1adaf9f68f4793df01b040005712ecf94740df5e
Christian Høj <cbh@ucn.dk>
2014-04-08 17:56:30 +0300

    Provide command line argument to take into account that the provided data files are formatted differently.
    Output usage description if needed parameters are not present.

--- convert_waze_data.rb
+++ convert_waze_data.rb
@@ -3,19 +3,29 @@
 
 require "csv"
 
+usage = %{
+convert_waze_data will convert Waze data collected with Mercator projection to WGS84 projection used by the GPS system.
+
+Script usage:
+convert_waze_data.rb <input file> <output file> <column index for first coordinate column>
+}
+
+abort usage if ARGV.length != 3
+
 mercator_file = ARGV[0]
 wgs84_out_file = ARGV[1]
+col = ARGV[2]
 tmp_file = "proj_tmp"
 tmp_wgs84_file = "proj_wgs84_tmp"
 
 # Extract locations from input file and write them to a temporary file
 File.open(tmp_file, "w") do |f|
   CSV.foreach mercator_file, {col_sep:"\t"} do |row|
-    if row[1].nil?
+    if row[col].nil? || !row[col].include?(',')
       f.puts "\n"
     else
-      row1 = row[1].sub(',', '.')
-      row2 = row[2].sub(',', '')
+      row1 = row[col].sub(',', '.')
+      row2 = row[col+1].sub(',', '')
       f.puts "#{row1}\t#{row2}\n"
     end
   end
@@ -28,8 +38,10 @@ line = 0
 converted = CSV.read tmp_wgs84_file, {col_sep:"\t"}
 CSV.open(wgs84_out_file, "w", {col_sep:"\t"}) do |outfile|
   CSV.foreach mercator_file, {col_sep:"\t"} do |row|
-    row[1] = converted[line][0]
-    row[2] = converted[line][1]
+    if !row[col].nil? && row[col].include?(',')
+      row[col] = converted[line][0]
+      row[col+1] = converted[line][1]
+    end
     outfile << row
     line += 1
   end
