5c125b3f31d1ef9db6da13ce0c9c24357ae559fc
Christian Høj <cbh@ucn.dk>
2014-04-10 15:43:34 +0300

    Code styled according to Rubocop

--- fetch_waze.rb
+++ fetch_waze.rb
@@ -1,7 +1,10 @@
 require 'http_request.rb'
-require 'json'
-require 'hashie'
 
+def write_file(content)
+  File.open("waze/#{Time.now.to_i}.json", 'w') do |f|
+    f.puts json_string
+  end
+end
 
 def fetch_waze_data
   uri = 'http://world.waze.com/rtserver/web/GeoRSS'
@@ -12,21 +15,11 @@ def fetch_waze_data
     top: 57.467857,
     bottom: 56.415208
   }
-  json_string = HttpRequest.get(url: uri, parameters: params).body
-  # hash = JSON.parse json_string
-  # msg = Hashie::Mash.new hash
-  # jams = msg.jams
-  # alerts = msg.alerts
-  # irregularities = msg.irregularities
-
-  File.open("waze/#{Time.now.to_i}.txt", "w") do |f|
-    f.puts json_string
-  end
+  write_file HttpRequest.get(url: uri, parameters: params).body
 end
 
-
-stop_time = Time.now + 24*60*60
+stop_time = Time.now + 24 * 60 * 60
 while stop_time > Time.now
   fetch_waze_data
-  sleep 10*60
+  sleep 10 * 60
 end
